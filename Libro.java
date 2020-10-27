586b286d551c354d426f50a876ef2c4e1624562c
Miguel Bayón <miguel.bayon@iessanandres.com>
2020-10-27 13:49:13 +0100

    Commit inicial

---
+++ Libro.java
@@ -0,0 +1,24 @@
+
+/**
+ * Una clase que representa objetos libro.
+ * Esta clase podria formar parte de un
+ * proyecto mas grande, como el de una aplicacion de
+ * una biblioteca
+ * 
+ * @author Miguel Bayon 
+ * @version 1.0
+ */
+public class Libro {
+
+    private String autor;
+    private String titulo;
+
+    /**
+     * Fija el autor y el titulo del libro a los dados como parametro
+     */
+    public Libro(String autorLibro, String tituloLibro)
+    {
+        autor = autorLibro;
+        titulo = tituloLibro;
+    }
+}
